05491dd854d506a3ad6aa8cb8384305dbdaa76a3
Rui Rosa Mendes <ruirosamendes@gmail.com>
2015-06-24 00:16:51 +0100

    Set Inspinia Template for this TechnologyScavenger Project

---
+++ TechnologyScavenger/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TechnologyScavenger.Startup))]
+namespace TechnologyScavenger
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
