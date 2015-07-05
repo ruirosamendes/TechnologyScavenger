c10b5667395c179663d522fe80721c8a3ed1cab6
Rui Rosa Mendes <ruirosamendes@gmail.com>
2015-07-05 14:31:31 +0100

    Load sites file into memory

---
+++ TechnologyScavenger.Service/SitesLoader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace TechnologyScavenger.Service
+{
+    public class SitesLoader
+    {
+        private FileStream sitesFile;
+
+        public SitesLoader(FileStream sitesFile)
+        {
+            this.sitesFile = sitesFile;
+        }
+    }
+}
